9948a84156546b28bbe4bcc2f6104eed2d6c00e4
takepro14 <takepro100pm@gmail.com>
2024-12-30 18:21:49 +0900

    feat(*): change to webpack based arch and untrack node_modules

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,68 @@
-import { TaskManager } from './task-manager';
+let table: HTMLTableElement;
+let message: HTMLInputElement;
 
-const taskManager = new TaskManager();
+function showTable(html: string) {
+  table.innerHTML = html;
+}
 
-console.log('TODOリストアプリが起動しました！');
+function doAction() {
+  const msg = message.value;
+  memo.add({ message: msg, date: new Date() });
+  memo.save();
+  memo.load();
+  showTable(memo.getHtml());
+}
 
-// タスク作成
-const task1 = taskManager.createTask('初めてのタスク');
-const task2 = taskManager.createTask('TypeScriptの学習');
+function doInitial() {
+  memo.data = [];
+  memo.save();
+  memo.load();
+  message.value = '';
+  showTable(memo.getHtml());
+}
 
-console.log('タスクが作成されました:');
-console.log(taskManager.getTasks());
+type Memo = {
+  message: string;
+  date: Date;
+};
 
-// タスク削除
-const deleteSuccess = taskManager.deleteTask(task1.id);
-console.log(
-  deleteSuccess ? 'タスクを削除しました:' : 'タスクが見つかりません:',
-);
+class MemoData {
+  data: Memo[] = [];
+
+  add(mm: Memo): void {
+    this.data.unshift(mm);
+  }
+
+  save(): void {
+    localStorage.setItem('memo_data', JSON.stringify(this.data));
+  }
+
+  load(): void {
+    const readed = JSON.parse(localStorage.getItem('memo_data'));
+    this.data = readed ? readed : [];
+  }
+
+  getHtml(): string {
+    let html = '<thead><th>Message</th><th>date</th></thead><tbody>';
+    for (let item of this.data) {
+      html +=
+        '<tr><td>' +
+        item.message +
+        '</td><td>' +
+        item.date.toLocaleString() +
+        '</td></tr>';
+    }
+    return html + '</tbody>';
+  }
+}
+
+const memo = new MemoData();
+
+window.addEventListener('load', () => {
+  table = document.querySelector('#table');
+  message = document.querySelector('#message');
+  document.querySelector('#btn').addEventListener('click', doAction);
+  document.querySelector('#initial').addEventListener('click', doInitial);
+  memo.load();
+  showTable(memo.getHtml());
+});
